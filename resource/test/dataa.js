6e9c1634ac4815b075684fdebd2d0c1e46727982
Lazyfarmerer <131164959+Lazyfarmerer@users.noreply.github.com>
2024-05-12 02:05:37 +0900

    테스트 중 오류 수정

---
+++ resource/test/dataa.js
@@ -0,0 +1,26 @@
+
+export class Data {
+    #data;
+
+    /**
+     * 정보를 저장해두고 사용하는 클래스
+     * @param {Object} jsonData 
+     */
+    constructor(jsonData) {
+        this.#data = jsonData;
+    }
+
+
+    get is_blog() { return this.#data.is_blog; }
+    set is_blog(value) {}
+    get post_title() { return this.#data.post_title; }
+    set post_title(value) {}
+    get blog_photo_url() { return this.#data.blog_photo_url; }
+    set blog_photo_url(value) {}
+    get personal_photo_url() { return this.#data.personal_photo_url; }
+    set personal_photo_url(value) {}
+    get time() { return this.#data.time; }
+    set time(value) {}
+    get user_time() { return this.#data.user_time; }
+    set user_time(value) {}
+}
